b6265d8cd5f95d7863ea1995e034523a67c06a87
tjsmith21 <35373339+tjsmith21@users.noreply.github.com>
2025-04-05 20:25:04 -0500

    Add files via upload
    v1.1

--- KragsMainBarHider/KragsMainBarHider.lua
+++ KragsMainBarHider/KragsMainBarHider.lua
@@ -1,46 +1,77 @@
 local f = CreateFrame("Frame")
 f:RegisterEvent("PLAYER_LOGIN")
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
+f:RegisterEvent("ZONE_CHANGED")
+f:RegisterEvent("ZONE_CHANGED_NEW_AREA")
 
 local function ApplyVisibilitySetting()
     C_Timer.After(0.1, function()
         ToggleKragsMainBar(KragsMainBarDB.hideBar)
         ToggleStanceBar(KragsMainBarDB.hideStanceBar)
+        ToggleBagsBar(KragsMainBarDB.hideBagsBar)
+        ToggleQuickJoinToastButton(KragsMainBarDB.hideQuickJoinToast)
     end)
 end
 
 f:SetScript("OnEvent", function()
     if not KragsMainBarDB then
-        KragsMainBarDB = { hideBar = true, hideStanceBar = true }  -- Add default setting for the stance bar
+        KragsMainBarDB = { 
+            hideBar = true, 
+            hideStanceBar = true, 
+            hideBagsBar = true,  -- Default setting for BagsBar
+            hideQuickJoinToast = true  -- Default setting for QuickJoinToastButton
+        }
     end
     ApplyVisibilitySetting()
 end)
 
 -- Toggle visibility of the main action bar
 function ToggleKragsMainBar(shouldHide)
+    KragsMainBarDB.hideBar = shouldHide
     if shouldHide then
         MainMenuBar:Hide()
-        print("Main action bar hidden.")
     else
         MainMenuBar:Show()
-        print("Main action bar shown.")
     end
-    KragsMainBarDB.hideBar = shouldHide
 end
 
 -- Toggle visibility of the stance bar
 function ToggleStanceBar(shouldHide)
-    -- Check if StanceBar exists and is not nil
     if StanceBar then
+        KragsMainBarDB.hideStanceBar = shouldHide
         if shouldHide then
             StanceBar:Hide()
-            print("Stance bar hidden.")
         else
             StanceBar:Show()
-            print("Stance bar shown.")
         end
-        KragsMainBarDB.hideStanceBar = shouldHide
-    else
-        print("Stance bar is not available for your class.")
+    end
+end
+
+-- Toggle visibility of the BagsBar (hides all bag buttons)
+function ToggleBagsBar(shouldHide)
+    local bagsBar = _G["BagsBar"]
+    if bagsBar then
+        KragsMainBarDB.hideBagsBar = shouldHide
+        if shouldHide then
+            BagsBar:Hide()
+        else
+            BagsBar:Show()
+        end
+    end
+end
+
+-- Hide QuickJoinToastButton and disable it
+function ToggleQuickJoinToastButton(shouldHide)
+    if QuickJoinToastButton then
+        KragsMainBarDB.hideQuickJoinToast = shouldHide
+        if shouldHide then
+            QuickJoinToastButton:Hide()
+            QuickJoinToastButton:UnregisterAllEvents()
+            QuickJoinToastButton:SetScript("OnShow", nil)
+        else
+            QuickJoinToastButton:Show()
+            QuickJoinToastButton:RegisterAllEvents()
+        end
     end
 end
 
@@ -49,6 +80,11 @@ SLASH_KRAGSMAINBAR1 = "/hidebar"
 SlashCmdList["KRAGSMAINBAR"] = function()
     local newState = not KragsMainBarDB.hideBar
     ToggleKragsMainBar(newState)
+    if newState then
+        print("Main action bar hidden.")
+    else
+        print("Main action bar shown.")
+    end
 end
 
 -- Slash command to toggle the stance bar
@@ -56,4 +92,33 @@ SLASH_KRAGSSTANCEBAR1 = "/hidestance"
 SlashCmdList["KRAGSSTANCEBAR"] = function()
     local newState = not KragsMainBarDB.hideStanceBar
     ToggleStanceBar(newState)
+    if newState then
+        print("Stance bar hidden.")
+    else
+        print("Stance bar shown.")
+    end
+end
+
+-- Slash command to toggle the bags bar
+SLASH_KRAGSBAGS1 = "/hidebags"
+SlashCmdList["KRAGSBAGS"] = function()
+    local newState = not KragsMainBarDB.hideBagsBar
+    ToggleBagsBar(newState)
+    if newState then
+        print("Bags bar hidden.")
+    else
+        print("Bags bar shown.")
+    end
+end
+
+-- Slash command to toggle the Quick Join Toast Button
+SLASH_KRAGSTOAST1 = "/hidetoast"
+SlashCmdList["KRAGSTOAST"] = function()
+    local newState = not KragsMainBarDB.hideQuickJoinToast
+    ToggleQuickJoinToastButton(newState)
+    if newState then
+        print("Quick Join Toast Button hidden.")
+    else
+        print("Quick Join Toast Button shown.")
+    end
 end
--- KragsMainBarHider/KragsMainBarHiderOptions.lua
+++ KragsMainBarHider/KragsMainBarHiderOptions.lua
@@ -13,7 +13,7 @@ local options = {
         description = {
             order = 1,
             type = "description",
-            name = "This addon hides the main action bar and the stance bar.",
+            name = "This addon hides the main action bar, the stance bar, and other elements.",
         },
         hideBar = {
             order = 2,
@@ -28,7 +28,7 @@ local options = {
                 return KMBH.db.profile.hideBar  -- Retrieve the setting from the profile
             end,
         },
-        hideStanceBar = {  -- New toggle for Stance Bar visibility
+        hideStanceBar = {  -- Toggle for Stance Bar visibility
             order = 3,
             type = "toggle",
             name = "Hide Stance Bar",
@@ -41,16 +41,44 @@ local options = {
                 return KMBH.db.profile.hideStanceBar  -- Retrieve the setting from the profile
             end,
         },
+        hideBagsBar = {  -- Toggle for Bags Bar visibility
+            order = 4,
+            type = "toggle",
+            name = "Hide Bags Bar",
+            desc = "Toggle visibility of the Bags Bar.",
+            set = function(info, val)
+                KMBH.db.profile.hideBagsBar = val  -- Store the setting in the profile
+                ToggleBagsBar(val)  -- Function to hide/show the Bags Bar
+            end,
+            get = function(info)
+                return KMBH.db.profile.hideBagsBar  -- Retrieve the setting from the profile
+            end,
+        },
+        hideQuickJoinToastButton = {  -- Toggle for Quick Join Toast Button visibility
+            order = 5,
+            type = "toggle",
+            name = "Hide Quick Join Toast Button",
+            desc = "Toggle visibility of the Quick Join Toast Button.",
+            set = function(info, val)
+                KMBH.db.profile.hideQuickJoinToast = val  -- Store the setting in the profile
+                ToggleQuickJoinToastButton(val)  -- Function to hide/show the Toast Button
+            end,
+            get = function(info)
+                return KMBH.db.profile.hideQuickJoinToast  -- Retrieve the setting from the profile
+            end,
+        },
     },
 }
 
 -- Function to initialize the addon and register the options
 function KMBH:OnInitialize()
-    -- Initialize the saved variables database with defaults for 'hideBar' and 'hideStanceBar'
+    -- Initialize the saved variables database with defaults
     self.db = AceDB:New("KMBH_DB", {
         profile = {
-            hideBar = true,        -- Default state for the action bar
-            hideStanceBar = true,  -- Default state for the stance bar
+            hideBar = true,           -- Default state for the action bar
+            hideStanceBar = true,     -- Default state for the stance bar
+            hideBagsBar = true,       -- Default state for the Bags Bar
+            hideQuickJoinToast = true,-- Default state for the Quick Join Toast Button
         },
     }, true)
     
